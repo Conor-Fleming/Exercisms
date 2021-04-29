4cf70ef6f707f46b844e741a59690a0e28d1cf77
Conor Fleming <conorfleming03@gmail.com>
2021-04-29 11:51:27 -0700

    made a change to account for utf-8 chars

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -5,12 +5,13 @@ import "fmt"
 
 //Distance function takes two strings and checks that they are the same length before counting the differences between the strings
 func Distance(a, b string) (int, error) {
-	if len(a) != len(b) {
+	runesA, runesB := []rune(a), []rune(b) //changed strings to runes to account for utf-8 characters (thanks andre)
+	if len(runesA) != len(runesB) {
 		return 0, fmt.Errorf("Two values %q, and %q are not the same length", a, b)
 	}
 	var count int = 0
-	for i := range a {
-		if a[i] != b[i] {
+	for i := range runesA {
+		if runesA[i] != runesB[i] {
 			count += 1
 		}
 	}
