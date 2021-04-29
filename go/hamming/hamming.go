ce2048e8e05add2293dc9ed2b77aaf95a2bf2ac5
Conor Fleming <conorfleming03@gmail.com>
2021-04-29 11:01:59 -0700

    inital solution to hamming problem

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,4 +1,16 @@
 package hamming
 
+import "fmt"
+
 func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return 0, fmt.Errorf("Two values %q, and %q are not the same length", a, b)
+	}
+	var count int = 0
+	for i := range a {
+		if a[i] != b[i] {
+			count += 1
+		}
+	}
+	return count, nil
 }
