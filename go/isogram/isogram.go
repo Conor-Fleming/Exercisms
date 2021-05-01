9fd51759da7d8b2ec47df33e63a8c8283453b51c
Conor Fleming <conorfleming03@gmail.com>
2021-05-01 08:57:29 -0700

    refactoring previous solution, had a couple lines that served no purpose

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -8,12 +8,8 @@ import (
 
 //IsIsogram takes a string as input and tells you if inputted string is an isogram
 func IsIsogram(input string) bool {
-	input = strings.Replace(input, "-", "", -1) //removing dashes
-	input = strings.Replace(input, " ", "", -1) //removing spaces
-	input = strings.ToLower(input)              //setting string to lower case
 	letters := make(map[rune]bool)
-	//fmt.Println(letters)
-	for _, val := range input {
+	for _, val := range strings.ToLower(input) {
 		if !unicode.IsLetter(val) { //checking for non letter chracters, if found skip
 			continue
 		}
@@ -23,6 +19,5 @@ func IsIsogram(input string) bool {
 			letters[val] = true //otherwise set letter to value true and add to map
 		}
 	}
-	//fmt.Println(letters)
 	return true
 }
