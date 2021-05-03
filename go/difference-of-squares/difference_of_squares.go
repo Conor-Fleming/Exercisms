c03fa033119d8999bf08154cc03cac5fa0624ca9
Conor Fleming <conorfleming03@gmail.com>
2021-05-03 07:34:31 -0700

    difference of squares with established formulas/simplified solution

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,20 +1,26 @@
 package diffsquares
 
 func SquareOfSum(input int) int {
-	result := 0
+	/*result := 0
 	for i := 0; i <= input; i++ {
 		result = i + result
 	}
-	return result * result
+	return result * result*/
+
+	//the above commented code is equivalent to the following formula
+	return input * input * (1 + input) * (1 + input) / 4
 }
 
 func SumOfSquares(input int) int {
-	result := 0
+	/*result := 0
 	for i := 0; i <= input; i++ {
 		sqr := i * i
 		result += sqr
 	}
-	return result
+	return result*/
+
+	//the above commented code is equivalent to the following formula
+	return input * (input + 1) * (2*input + 1) / 6
 }
 
 func Difference(input int) int {
