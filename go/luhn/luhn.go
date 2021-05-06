a254d4a14a0c9d02d26afb53307722f7a60a7529
Conor Fleming <conorfleming03@gmail.com>
2021-05-06 13:26:26 -0700

    working solution v1

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -16,14 +16,25 @@ func Valid(number string) bool {
 	//checking for non digit characters
 	totalSum := 0
 	for i := range numbers {
+		//checking for non-digit chars
 		if !unicode.IsDigit(numbers[i]) {
 			return false
 		}
 		v := int(numbers[i] - '0')
-		if i%2 == 0 {
-			v = v * 2
-			if v > 9 {
-				v = v - 9
+		//determine which chars to double based on length of number
+		if len(numbers)%2 == 0 {
+			if i%2 == 0 {
+				v = v * 2
+				if v > 9 {
+					v = v - 9
+				}
+			}
+		} else {
+			if i%2 != 0 {
+				v = v * 2
+				if v > 9 {
+					v = v - 9
+				}
 			}
 		}
 		totalSum += v
