6455852c35688fb48c7d061b8b1c994534727b3e
Conor Fleming <conorfleming03@gmail.com>
2021-05-09 18:33:49 -0700

    revisting, not passing all tests

--- cpp/bob/bob.cpp
+++ cpp/bob/bob.cpp
@@ -8,12 +8,12 @@ using std::string;
 
 namespace bob {
 string hey(string input) { 
-    if(isupper(input[1])){
+    if(isupper(input[2])){
         if(input[input.length()] == '?')
             return "Calm down, I know what I'm doing!";
         else
            return "Whoa, chill out!";
-    }else if(!isupper(input[1])){
+    }else if(!isupper(input[2] || (input[1] != ' ' && input[input.length()] != ' '))){
         if(input[input.length()] == '?')
              return "Sure.";
         else
