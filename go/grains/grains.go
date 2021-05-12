37cc2502b8b67a1b7d0c89658d5639f704d7fc57
Conor Fleming <conorfleming03@gmail.com>
2021-05-11 21:31:33 -0700

    need to read up on bitwise ops

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -1,13 +1,16 @@
 package grains
 
-func Square(square int) int {
-	amount := 0
+import (
+	"errors"
+)
 
-	return amount
+func Square(input int) (uint64, error) {
+	if input < 1 || input > 64 {
+		return 0, errors.New("input must be greater than 0 and less than 65")
+	}
+	return (1 << uint64(input-1)), nil
 }
 
-func Total() int {
-	total := 0
-
-	return total
+func Total() uint64 {
+	return 1<<64 - 1
 }
