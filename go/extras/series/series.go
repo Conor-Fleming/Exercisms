7b8ee29bc7a46ef2edd1bcd7d274f8a2778ad04d
Conor Fleming <conorfleming03@gmail.com>
2021-06-22 16:59:42 -0700

    series exercise

--- go/extras/series/series.go
+++ go/extras/series/series.go
@@ -1,1 +1,17 @@
 package series
+
+//All function returns a list of all substrings of s with length n
+func All(n int, s string) []string {
+	var result []string
+
+	return result
+}
+
+//UnsafeFirst func returns the first substring of s with length n
+func UnsafeFirst(n int, s string) string {
+	var result string
+	for i := 0; i < n; i++ {
+		result = result + string(s[i])
+	}
+	return result
+}
