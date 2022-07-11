83654da993784f2ccfa7d3ba37edf8b309a3ad6c
Conor Fleming <32988273+Conor-Fleming@users.noreply.github.com>
2022-07-10 22:08:45 -0700

    will need to revisit to update line 17 to use function

--- go/cars-assemble/cars_assemble.go
+++ go/cars-assemble/cars_assemble.go
@@ -10,10 +10,18 @@ func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float6
 // CalculateWorkingCarsPerMinute calculates how many working cars are
 // produced by the assembly line every minute
 func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
-	return (CalculateWorkingCarsPerMinute(productionRate, successRate) / 60)
+	//This line giving me issues but not sure why. Will just re-calculate the Per Hour number again within this function
+	//When I learn the cause of error, and address I will re-enstate the use of the function
+	//perHour := CalculateWorkingCarsPerMinute(productionRate, successRate)
+
+	perHour := (float64(successRate/100) * float64(productionRate))
+	perminute := int(perHour) / 60
+	return perminute
 }
 
 // CalculateCost works out the cost of producing the given number of cars
 func CalculateCost(carsCount int) uint {
-	return uint(10)
+	indivs := carsCount % 10
+	tenners := carsCount / 10
+	return (uint((indivs * 10000) + (tenners * 95000)))
 }
