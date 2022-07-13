1d88863cfbabf9a164e8b8ac06cf26004c3eb3a8
Conor Fleming <32988273+Conor-Fleming@users.noreply.github.com>
2022-07-13 07:47:36 -0700

    messing with strings in Go

--- go/welcome-to-tech-palace/welcome_to_tech_palace.go
+++ go/welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -7,17 +7,19 @@ import (
 
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
-	output := "Welcome to Tech Palace," + strings.ToUpper(customer)
+	output := "Welcome to the Tech Palace, " + strings.ToUpper(customer)
 	return output
 }
 
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
-	border := strings.Repeat("*", numStarsPerLine)
-	return fmt.Println(border, '\n', welcomeMsg, '\n', border)
+	border := fmt.Sprintf(strings.Repeat("*", numStarsPerLine))
+	return fmt.Sprint(border, "\n", welcomeMsg, "\n", border)
 }
 
 // CleanupMessage cleans up an old marketing message.
 func CleanupMessage(oldMsg string) string {
-	panic("Please implement the CleanupMessage() function")
+	newMsg := strings.ReplaceAll(oldMsg, "*", "")
+	newMsg = strings.TrimSpace(newMsg)
+	return newMsg
 }
