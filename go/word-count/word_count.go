78e42659622b54e88df4f616ecc08448ebb4db59
Conor Fleming <32988273+Conor-Fleming@users.noreply.github.com>
2022-07-17 21:24:07 -0700

    all tests passing but last

--- go/word-count/word_count.go
+++ go/word-count/word_count.go
@@ -7,12 +7,17 @@ import (
 type Frequency map[string]int
 
 func WordCount(phrase string) Frequency {
+	phrase = strings.ToLower(phrase)
+	lastChar := phrase[len(phrase)-1]
+	if lastChar == "'" {
+
+	}
 	result := make(map[string]int)
-	noPunc := strings.NewReplacer(",", ".", ";", "/")
+	noPunc := strings.NewReplacer("' ", " ", " '", " ", ",", " ", ".", " ", ";", " ", "/", " ", ":", "", "!", "", "&", "", "@", "", "$", "", "%", "", "^", "", "' ", "")
 	phrase = noPunc.Replace(phrase)
 	words := strings.Fields(phrase)
 	for _, word := range words {
-		if _, ok := result[word]; ok {
+		if result[word] == 0 {
 			result[word] = 1
 		} else {
 			result[word]++
