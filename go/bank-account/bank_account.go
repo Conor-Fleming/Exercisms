df8d7fa6f3f7bf82d3fa5831156b7ad82833b711
Conor Fleming <32988273+Conor-Fleming@users.noreply.github.com>
2022-08-02 22:06:09 -0700

    set up mutex and balance deposit and close functions

--- go/bank-account/bank_account.go
+++ go/bank-account/bank_account.go
@@ -1,10 +1,21 @@
 package account
 
+import "sync"
+
 // Define the Account type here.
 
 type Account struct {
-	AcctBalance int64
-	Closed      bool
+	acctBalance int64
+	closed      bool
+	mux         *sync.Mutex
+}
+
+func (a Account) Lock() {
+	a.mux.Lock()
+}
+
+func (a Account) Unlock() {
+	a.mux.Unlock()
 }
 
 func Open(amount int64) *Account {
@@ -13,23 +24,43 @@ func Open(amount int64) *Account {
 		return nil
 	}
 	newAccount := Account{
-		AcctBalance: amount,
-		Closed:      false,
+		acctBalance: amount,
+		closed:      false,
+		mux:         &sync.Mutex{},
 	}
 	return &newAccount
 }
 
 func (a *Account) Balance() (int64, bool) {
 	//panic("Please implement the Balance function")
-	return 0.0, false
+	a.Lock()
+	defer a.Unlock()
+	if a.closed == true {
+		return 0, false
+	}
+	return a.acctBalance, true
 }
 
 func (a *Account) Deposit(amount int64) (int64, bool) {
 	//panic("Please implement the Deposit function")
-	return 0.0, false
+	a.Lock()
+	defer a.Unlock()
+	if a.closed == true {
+		return 0, false
+	}
+	a.acctBalance += amount
+	return a.acctBalance, true
 }
 
 func (a *Account) Close() (int64, bool) {
-	//panic("Please implement the Close function")
-	return 0.0, false
+	a.Lock()
+	defer a.Unlock()
+	if a.closed == true {
+		return 0, false
+	}
+	endingBalance := a.acctBalance
+	a.acctBalance = 0
+	a.closed = true
+	return endingBalance, true
+
 }
