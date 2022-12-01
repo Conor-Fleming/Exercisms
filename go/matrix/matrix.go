d70a9f56c36111ee2afad2d45b5f505eff0cfd6e
Conor Fleming <conorfleming03@gmail.com>
2022-11-30 22:22:22 -0800

    populating matrix

--- go/matrix/matrix.go
+++ go/matrix/matrix.go
@@ -1,29 +1,36 @@
 package matrix
 
+import (
+	"strconv"
+	"strings"
+)
+
 type Matrix [][]int
 
 func New(s string) (*Matrix, error) {
+	rows := strings.Split(s, " ")
 	var matrix Matrix
-	for i, val := range s {
-		y := 0
-		if val == '\n' {
-			i++
-			continue
+	for i := 0; i < len(rows)/2; i++ {
+		for y := 0; y < len(rows)/2; y++ {
+			value, err := strconv.Atoi(rows[i])
+			if err != nil {
+				return nil, err
+			}
+			matrix[i][y] = value
 		}
-		matrix[y][i] = int(val)
 	}
 	return &matrix, nil
 }
 
 // Cols and Rows must return the results without affecting the matrix.
 func (m *Matrix) Cols() [][]int {
-	panic("Please implement the Cols function")
+	return nil
 }
 
 func (m *Matrix) Rows() [][]int {
-	panic("Please implement the Rows function")
+	return nil
 }
 
 func (m *Matrix) Set(row, col, val int) bool {
-	panic("Please implement the Set function")
+	return true
 }
