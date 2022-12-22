ac9b473118a313efe2bdb4d3c557f7e19b4f9093
Conor-Fleming <conorfleming03@gmail.com>
2022-12-21 18:48:18 -0800

    comments

--- go/matrix/matrix.go
+++ go/matrix/matrix.go
@@ -8,13 +8,20 @@ import (
 type Matrix [][]int
 
 func New(s string) (*Matrix, error) {
+	//need to do some error checking for:
+	// int overflows (long integers), uneven rows, empty rows, non ints and non numerics
+	//basic string manipulation to enable easy parsing
 	s = strings.ReplaceAll(s, "\n", " . ")
 	rowCount := strings.Count(s, ".")
 	lines := strings.Split(s, " ")
+
+	//create matrix so store values
 	mx := make(Matrix, rowCount+1)
 
 	i := 0
+	//loop range of string and add to matrix row
 	for _, val := range lines {
+		// the current value is the newline marker (".") then increment matrix row with i++
 		if val == "." {
 			i++
 			continue
@@ -22,8 +29,6 @@ func New(s string) (*Matrix, error) {
 		val, _ := strconv.Atoi(string(val))
 		mx[i] = append(mx[i], val)
 	}
-
-	//fmt.Println(mx)
 
 	return &mx, nil
 }
