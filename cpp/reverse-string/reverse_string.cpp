1f75dc50b340e69bb4864e2d0b3fc782393a9a5c
Conor-Fleming <conorfleming03@gmail.com>
2023-01-31 18:03:17 -0800

    solution

--- cpp/reverse-string/reverse_string.cpp
+++ cpp/reverse-string/reverse_string.cpp
@@ -1,5 +1,15 @@
 #include "reverse_string.h"
 
-namespace reverse_string {
+namespace reverse_string
+{
+    string reverse_string(string input)
+    {
+        string result = "";
+        for (int i = input.length(); i > 0; i--)
+        {
+            result += input[i];
+        }
 
-}  // namespace reverse_string
+        return result;
+    }
+} // namespace reverse_string
--- cpp/reverse-string/reverse_string.h
+++ cpp/reverse-string/reverse_string.h
@@ -1,8 +1,12 @@
 #if !defined(REVERSE_STRING_H)
 #define REVERSE_STRING_H
 
-namespace reverse_string {
+#include <string>
+using namespace std;
 
-}  // namespace reverse_string
+namespace reverse_string
+{
+    string reverse_string(string);
+} // namespace reverse_string
 
 #endif // REVERSE_STRING_H
