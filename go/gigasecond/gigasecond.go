e29e9374d728aa7a5c65b873c007a17bf0195490
Conor-Fleming <conorfleming03@gmail.com>
2023-11-05 15:55:21 -0700

    starting point

---
+++ go/gigasecond/gigasecond.go
@@ -0,0 +1,11 @@
+// Package gigasecond
+package gigasecond
+
+// import path for the time package from the standard library
+import "time"
+
+// AddGigasecond takes a value of type time.Time, t and returns the time.Time plus on Gigasecond .
+func AddGigasecond(t time.Time) time.Time {
+
+	return t
+}
