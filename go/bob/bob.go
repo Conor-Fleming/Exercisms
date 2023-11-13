db62ea81382a10a6963c1b3d88f0790821587497
Conor-Fleming <conorfleming03@gmail.com>
2023-11-13 16:20:37 -0700

    tiding up passing solution

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -8,14 +8,17 @@ import (
 
 // Hey allows bob to say hey
 func Hey(remark string) string {
+	//cleaning string
+	remark = strings.TrimSpace(remark)
+
 	switch {
-	case strings.TrimSpace(remark) == "":
+	case remark == "":
 		return "Fine. Be that way!"
+	case isUpperWithLetters(remark) && strings.HasSuffix(remark, "?"):
+		return "Calm down, I know what I'm doing!"
 	case strings.HasSuffix(remark, "?"):
 		return "Sure."
-	case isUpper(remark) && strings.HasSuffix(remark, "?"):
-		return "Calm down, I know what I'm doing!"
-	case isUpper(remark):
+	case isUpperWithLetters(remark):
 		return "Whoa, chill out!"
 	default:
 		return "Whatever."
@@ -24,9 +27,24 @@ func Hey(remark string) string {
 
 func isUpper(s string) bool {
 	for _, v := range s {
-		if !unicode.IsUpper(v) {
+		if !unicode.IsUpper(v) && unicode.IsLetter(v) {
 			return false
 		}
 	}
+
 	return true
+}
+
+func hasLetters(s string) bool {
+	for _, v := range s {
+		if unicode.IsLetter(v) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func isUpperWithLetters(s string) bool {
+	return isUpper(s) && hasLetters(s)
 }
