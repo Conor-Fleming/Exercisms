95ee14f7192ff1dc7698f01e1b015db34f7b58ba
Conor-Fleming <conorfleming03@gmail.com>
2024-04-30 21:41:27 -0700

    generate sum func

--- go/dnd-character/dnd_character.go
+++ go/dnd-character/dnd_character.go
@@ -1,5 +1,9 @@
 package dndcharacter
 
+import (
+	"math/rand"
+)
+
 type Character struct {
 	Strength     int
 	Dexterity    int
@@ -22,6 +26,42 @@ func Ability() int {
 
 // GenerateCharacter creates a new Character with random scores for abilities
 func GenerateCharacter() Character {
+	character := Character{
+		Strength:     GenerateSum(),
+		Dexterity:    GenerateSum(),
+		Constitution: GenerateSum(),
+		Intelligence: GenerateSum(),
+		Wisdom:       GenerateSum(),
+		Charisma:     GenerateSum(),
+	}
 
-	return Character{}
+	character.Hitpoints = int(10 + float64((character.Constitution-10)/2))
+	return character
+}
+
+func GenerateSum() int {
+	rollValues := make([]int, 4)
+
+	//4 rolls of the dice
+	rollValues = append(rollValues, rand.Intn(6))
+	rollValues = append(rollValues, rand.Intn(6))
+	rollValues = append(rollValues, rand.Intn(6))
+	rollValues = append(rollValues, rand.Intn(6))
+
+	min := 6 //highest possible dice roll
+	indextoRemove := 0
+	for i, v := range rollValues {
+		if v <= min {
+			indextoRemove = i
+		}
+	}
+
+	rollValues = append(rollValues[:indextoRemove], rollValues[indextoRemove+1:]...) //removing minumum value
+
+	score := 0
+	for _, v := range rollValues {
+		score = score + v
+	}
+
+	return score
 }
