699cfec25e908854fd482d6d993777be08f2a309
Conor-Fleming <conorfleming03@gmail.com>
2024-09-29 21:01:12 -0700

    im back

--- go/nth-prime/nth_prime.go
+++ go/nth-prime/nth_prime.go
@@ -1,6 +1,44 @@
 package prime
 
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
 // Nth returns the nth prime number. An error must be returned if the nth prime number can't be calculated ('n' is equal or less than zero)
 func Nth(n int) (int, error) {
-	panic("Please implement the Nth function")
+	if n <= 0 {
+		return 0, errors.New("Nth prime cannot be computed, input was less than 1")
+	}
+
+	primes := []int{}
+	i := 2
+	for {
+		if len(primes) == n {
+			break
+		}
+
+		if IsPrime(float64(i)) {
+			primes = append(primes, i)
+		}
+		i++
+	}
+	fmt.Println(primes)
+
+	return primes[len(primes)-1], nil
+}
+
+func IsPrime(n float64) bool {
+	if n <= 1 {
+		return false
+	}
+
+	for i := 2; float64(i) < math.Sqrt(n); i++ {
+		if int(n)%i == 0 {
+			return false
+		}
+	}
+
+	return true
 }
